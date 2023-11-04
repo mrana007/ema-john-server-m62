57756c8c7b2956e6caae1615dfc763d02010e8d1
mrana007 <rana.es.bc@gmail.com>
2023-11-04 17:21:48 +0600

    loaed pagination data from backend

--- index.js
+++ index.js
@@ -30,7 +30,14 @@ async function run() {
     const productCollection = client.db("emaJohnDB").collection("products");
 
     app.get("/products", async (req, res) => {
-      const result = await productCollection.find().toArray();
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      
+      console.log('pagination query', page, size);
+      const result = await productCollection.find()
+      .skip(page * size)
+      .limit(size)
+      .toArray();
       res.send(result);
     });
     
