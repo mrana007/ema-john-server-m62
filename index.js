cb6da787109779645fa37c0f074a8847c047bc95
mrana007 <rana.es.bc@gmail.com>
2023-11-04 18:18:00 +0600

    fixed orders bug

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4kezvwg.mongodb.net/?retryWrites=true&w=majority`;
@@ -32,12 +32,25 @@ async function run() {
     app.get("/products", async (req, res) => {
       const page = parseInt(req.query.page);
       const size = parseInt(req.query.size);
-      
+
       console.log('pagination query', page, size);
       const result = await productCollection.find()
       .skip(page * size)
       .limit(size)
       .toArray();
+      res.send(result);
+    });
+
+    app.post('/productsByIds', async(req, res)=>{
+      const ids = req.body;
+      const idsWithObjectId = ids.map(id => new ObjectId(id))
+      const query ={
+        _id:{
+          $in:idsWithObjectId
+        }
+      }
+      const result = await productCollection.find(query).toArray();
+      console.log(idsWithObjectId);
       res.send(result);
     });
     
